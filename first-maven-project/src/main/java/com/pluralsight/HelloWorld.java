7585291a838613a1a505556460e4b0c9039a964a
Eden Mengistu <eden.g.mengistu@gmail.com>
2025-04-10 11:29:29 -0500

    completed shoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+
+
+
+
+}
---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("shopping List:");
+        System.out.println("apple:");
+        System.out.println("banana:");
+        System.out.println("orange:");
+        System.out.println("carrot:");
+        System.out.println("kiwi:") ;
+        System.out.println("blueberry:");
+        System.out.println("strawberry:");
+        System.out.println("cabbage:");
+    }
+
+
+
+
+
+
+}
